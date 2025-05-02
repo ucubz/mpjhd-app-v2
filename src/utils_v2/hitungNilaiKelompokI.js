425f17f0fff47e46e32d15778e6e29d420f15374
ucubz <ucubz@yahoo.com>
2025-05-02 21:04:59 +0700

    Update hitungNilaiKelompokI.js
    perbaiki lebih dari 29 nilai 90

--- src/utils_v2/hitungNilaiKelompokI.js
+++ src/utils_v2/hitungNilaiKelompokI.js
@@ -4,7 +4,7 @@ export function hitungNilaiKelompokI(jumlahHari, apakahBerturut = false) {
   const hari = parseInt(jumlahHari);
   if (isNaN(hari) || hari <= 0) return 0;
 
-  if (apakahBerturut && hari === 10) return 90; // prioritas untuk 10 hari berturut-turut
+  if (apakahBerturut && hari === 10) return 90;
   if (hari === 3) return 10;
   if (hari >= 4 && hari <= 6) return 20;
   if (hari >= 7 && hari <= 10) return 30;
@@ -13,7 +13,7 @@ export function hitungNilaiKelompokI(jumlahHari, apakahBerturut = false) {
   if (hari >= 17 && hari <= 20) return 60;
   if (hari >= 21 && hari <= 24) return 70;
   if (hari >= 25 && hari <= 27) return 80;
-  if (hari === 28) return 90;
+  if (hari >= 28) return 90; // berlaku untuk 28 ke atas
 
   return 0;
 }
